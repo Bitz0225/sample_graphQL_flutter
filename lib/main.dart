2dd6b2f30ac0bf4fea108f160dcbbea841dfacb6
thaipq <pqthai2502@gmail.com>
2023-12-06 21:56:16 +0700

    change into using Query widget

--- lib/main.dart
+++ lib/main.dart
@@ -58,31 +58,80 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<dynamic> characters = [];
-  bool _loading = false;
+  late ValueNotifier<GraphQLClient> client;
+  bool isFetch = false;
+
+  @override
+  void initState() {
+    final link = HttpLink(
+      'https://rickandmortyapi.com/graphql',
+    );
+     client = ValueNotifier(
+      GraphQLClient(
+        link: link,
+        // The default store is the InMemoryStore, which does NOT persist to disk
+        cache: GraphQLCache(store: HiveStore()),
+      ),
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _loading
-          ? const CircularProgressIndicator()
-          : characters.isEmpty
-              ? Center(
-                  child: ElevatedButton(
-                    child: const Text("Fetch Data"),
-                    onPressed: () {
-                      fetchData();
-                    },
+    return GraphQLProvider(
+      client: client,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Stack(
+          children: [
+            Visibility(
+              visible: !isFetch,
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      isFetch = true;
+                    });
+                  },
+                  child: const Text('Fetch'),
+                ),
+              ),
+            ),
+            Visibility(
+              visible: isFetch,
+              child: Query(
+                options: QueryOptions(
+                  document: gql(
+                    """query {
+  characters() {
+      results {
+              name
+              image
+              gender
+      }
+  }
+
+}""",
                   ),
-                )
-              : Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListView.builder(
+                ),
+                builder: (QueryResult<Object?> result,
+                    {Future<QueryResult<Object?>> Function(
+                        FetchMoreOptions)?
+                    fetchMore,
+                      Future<QueryResult<Object?>?> Function()? refetch}) {
+                  if (result.hasException) {
+                    return Text(result.exception.toString());
+                  }
+                  if (result.isLoading) {
+                    return const CircularProgressIndicator();
+                  }
+                  final List<dynamic> characters = result.data?['characters']['results'];
+                  return ListView.builder(
+                      padding: const EdgeInsets.all(8),
                       itemCount: characters.length,
-                      itemBuilder: (context, index) {
+                      itemBuilder: (BuildContext context, int index) {
                         return Card(
                           child: ListTile(
                             leading: Image(
@@ -95,47 +144,13 @@ class _MyHomePageState extends State<MyHomePage> {
                             ),
                           ),
                         );
-                      }),
-                ),
-    );
-  }
-
-  void fetchData() async {
-    setState(() {
-      _loading = true;
-    });
-    HttpLink link = HttpLink("https://rickandmortyapi.com/graphql");
-    GraphQLClient qlClient = GraphQLClient(
-      link: link,
-      cache: GraphQLCache(
-        store: HiveStore(),
-      ),
-    );
-    QueryResult queryResult = await qlClient.query(
-      QueryOptions(
-        document: gql(
-          """query {
-  characters() {
-    results {
-      name
-      image 
-    }
-  }
-  
-}""",
+                      });
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
-
-// queryResult.data  // contains data
-// queryResult.exception // will give what exception you got /errors
-// queryResult.hasException // you can check if you have any exception
-
-// queryResult.context.entry<HttpLinkResponseContext>()?.statusCode  // to get status code of response
-
-    setState(() {
-      characters = queryResult.data!['characters']['results'];
-      _loading = false;
-    });
   }
 }
